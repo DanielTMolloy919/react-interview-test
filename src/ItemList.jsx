e23e7f5e7616c1ec552272d3f881192c97e5ffa9
Daniel Molloy <danieltmolloy919@gmail.com>
2025-04-14 12:20:15 +1000

    remove more comments

--- src/ItemList.jsx
+++ src/ItemList.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-// Simple component, assumes keys will be handled correctly in App.js map
 function ItemList({ items }) {
   if (!items || items.length === 0) {
     return <p>No items match the filter.</p>;
@@ -8,7 +7,6 @@ function ItemList({ items }) {
   return (
     <ul>
       {items.map((item) => (
-        // We'll add the key here now as it's not the bug focus
         <li key={item.id}>{item.name}</li>
       ))}
     </ul>
